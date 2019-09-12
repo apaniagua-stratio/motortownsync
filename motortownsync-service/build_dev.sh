bf4fe7f49318eccd7f4ac33303c4cd9b7446aa6f
Alvaro Paniagua Moreno <apaniagua@stratio.com>
2019-09-12 13:27:01 +0200

    change FROM image and build script

--- motortownsync-service/build_dev.sh
+++ motortownsync-service/build_dev.sh
@@ -3,6 +3,6 @@ gradle clean assemble
 mkdir build/resources/main/lib
 cp build/libs/*.jar build/resources/main/lib
 cd build/resources/main
-docker build -t registry.anjana.local:5001/motortownsync:1.1.0-SNAPSHOT-2 .
-docker push registry.anjana.local:5001/motortownsync:1.1.0-SNAPSHOT-2
+docker build -t registry.anjana.local:5001/motortownsync:1.1.0-SNAPSHOT-3 .
+docker push registry.anjana.local:5001/motortownsync:1.1.0-SNAPSHOT-3
 
